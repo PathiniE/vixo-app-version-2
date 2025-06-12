db1738d75e9894940b644067e1e1de6beb6d1743
Pathini Ekanayaka <pathini2002@gmail.com>
2025-06-12 10:50:57 +0530

    change the text position

--- src/components/lottery/LotteryResults.tsx
+++ src/components/lottery/LotteryResults.tsx
@@ -117,16 +117,16 @@ const LotteryResults: React.FC<LotteryResultsProps> = ({
     <div className="bg-[#191919] min-h-screen">
       <div className="max-w-md mx-auto">
         <Tabs defaultValue="results" className="w-full">
-          <TabsList className="flex w-full bg-transparent rounded-none h-10 gap-4 justify-start lest-0">
+          <TabsList className="flex w-full bg-transparent rounded-none h-10 justify-start p-0 relative">
             <TabsTrigger
               value="results"
-              className="relative bg-transparent text-white text-xl shadow-none data-[state=active]:bg-transparent data-[state=active]:shadow-none rounded-none pb-3 data-[state=active]:after:content-[''] data-[state=active]:after:absolute data-[state=active]:after:bottom-0 data-[state=active]:after:left-1/2 data-[state=active]:after:-translate-x-1/2 data-[state=active]:after:w-8 data-[state=active]:after:h-0.5 data-[state=active]:after:bg-[#E25319] data-[state=active]:after:rounded-full"
+              className=" bg-transparent text-[#FFFFFF80] data-[state=active]:text-white text-xl shadow-none data-[state=active]:bg-transparent data-[state=active]:shadow-none rounded-none pb-3 px-0 absolute left-0 data-[state=active]:after:content-[''] data-[state=active]:after:absolute data-[state=active]:after:bottom-0 data-[state=active]:after:left-1/2 data-[state=active]:after:-translate-x-1/2 data-[state=active]:after:w-8 data-[state=active]:after:h-0.5 data-[state=active]:after:bg-[#E25319] data-[state=active]:after:rounded-full"
             >
               Results
             </TabsTrigger>
             <TabsTrigger
               value="my-winnings"
-              className="relative bg-transparent text-white text-xl shadow-none data-[state=active]:bg-transparent data-[state=active]:shadow-none rounded-none pb-3 data-[state=active]:after:content-[''] data-[state=active]:after:absolute data-[state=active]:after:bottom-0 data-[state=active]:after:left-1/2 data-[state=active]:after:-translate-x-1/2 data-[state=active]:after:w-8 data-[state=active]:after:h-0.5 data-[state=active]:after:bg-[#E25319] data-[state=active]:after:rounded-full"
+              className=" bg-transparent text-[#FFFFFF80] data-[state=active]:text-white text-xl shadow-none data-[state=active]:bg-transparent data-[state=active]:shadow-none rounded-none pb-3 px-0 absolute left-30 data-[state=active]:after:content-[''] data-[state=active]:after:absolute data-[state=active]:after:bottom-0 data-[state=active]:after:left-1/2 data-[state=active]:after:-translate-x-1/2 data-[state=active]:after:w-8 data-[state=active]:after:h-0.5 data-[state=active]:after:bg-[#E25319] data-[state=active]:after:rounded-full"
             >
               My Winnings
             </TabsTrigger>
@@ -154,10 +154,3 @@ const LotteryResults: React.FC<LotteryResultsProps> = ({
 };
 
 export default LotteryResults;
-
-
-
-
-
-
-          
